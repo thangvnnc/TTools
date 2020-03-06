79377cc72328ef5cb8b7e8b3b856db6d622566bb
unknown <thangvnnc@gmail.com>
2020-03-06 14:05:16 +0700

    cm

---
+++ TTools.bat
@@ -0,0 +1,6 @@
+@echo off
+D:
+cd D:\Thang PC\Nodejs-CodeSupport
+node index.js
+pause
+exit
---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const sqlFormatter = require('sql-formatter');
+const app = express();
+const bodyParser = require('body-parser');
+const chromeLauncher = require('chrome-launcher');
+const PORT = 1111;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function(err) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    console.log('Server started in port: ' + PORT);
+    
+    chromeLauncher.launch({
+        startingUrl: 'http://localhost:'+PORT
+    }).then(chrome => {
+        console.log(`Chrome debugging port running on ${chrome.port}`);
+    });
+})
+
+app.post('/sql/build', function (req, res) {
+    let data = req.body;
+    let start = data.start;
+    let end = data.end;
+    let indent = data.indent;
+    let contentReq = data.content;
+    let sqlFomat = formatSql(contentReq, indent);
+
+    let sqlResult = '';
+    let lines = sqlFomat.split('\n');
+    for(let idxLine = 0; idxLine < lines.length; idxLine++) {
+        sqlResult += start + lines[idxLine] + end + '\n';
+    }
+
+    let result = {
+        code: 0,
+        content: sqlResult
+    }
+
+    res.send(result);
+});
+
+function formatSql(sql, indent) {
+    let numberSpace = '';
+    for(let idx = 0; idx < parseInt(indent); idx++) {
+        numberSpace += ' ';
+    }
+
+    return sqlFormatter.format(sql, {
+        language: 'n1ql',
+        indent: numberSpace
+    })
+}
